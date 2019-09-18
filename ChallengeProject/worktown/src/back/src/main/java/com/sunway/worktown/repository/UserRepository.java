0df1de39f3520eca71a645c185250c9c70f242b3
minove <2368157914@qq.com>
2019-09-18 11:17:29 +0800

    创建Repository

---
+++ ChallengeProject/worktown/src/back/src/main/java/com/sunway/worktown/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.sunway.worktown.repository;
+
+import com.sunway.worktown.entity.UserEntity;
+
+/**
+ * 用户数据仓库
+ *
+ * @author malin
+ * @version 1.0
+ */
+public interface UserRepository extends BaseRepository<UserEntity> {
+}
